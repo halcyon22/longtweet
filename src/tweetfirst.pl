589456a1da2349fd1492a0170ed4b65f33443ee1
mpartridge <halcyon22@gmail.com>
2011-11-09 22:30:01 -0600

    sort files in given dir by numbers just before the extension tweet first file using OAuth keys in user's ~/.twitter/ dir tested working needs to move the tweeted file to avoid duplicate tweet

--- src/tweetfirst.pl
+++ src/tweetfirst.pl
@@ -1,28 +1,54 @@
 #!/usr/bin/perl -w
 
 use strict;
+use File::Slurp;
+use File::HomeDir;
+use Net::Twitter::Lite;
 
 if (!$ARGV[0]) {
 	die <<USAGE;
 Usage $0 <directory>
-	directory: directory from which the contents of the first file will be tweeted
+	directory: path to look for tweet files
 USAGE
-	#exit;
 }
 
 my $srcdir = shift(@ARGV);
 
 opendir(DIR, $srcdir)
-	or die $!;
+	or die "$! \"$srcdir\"";
 
-my @files = grep {
-	/\.tweet$/ && -f "$srcdir/$_"
-	} readdir(DIR);
+# list files sorted by numbers just before the .tweet extension
+my @files = 
+	map { $_->[1] }
+	sort { $a->[0] <=> $b->[0] } 
+	map {/(\d*?)\.tweet/; [$1, $_] }
+	grep { /\.tweet$/ && -f "$srcdir/$_" } 
+	readdir(DIR);
 
 closedir(DIR);
 
-foreach my $file (@files) {
-	print "$file\n";
-}
+my $firstfile = shift(@files);
+my $tweet = File::Slurp::read_file("$srcdir/$firstfile");
 
-exit;
+my $homedir = File::HomeDir->my_home;
+my $consumer_key = File::Slurp::read_file($homedir."/.twitter/consumer_key");
+my $consumer_secret = File::Slurp::read_file($homedir."/.twitter/consumer_secret");
+my $access_token = File::Slurp::read_file($homedir."/.twitter/access_token");
+my $access_token_secret = File::Slurp::read_file($homedir."/.twitter/access_token_secret");
+
+my $twitter = Net::Twitter::Lite->new(
+	consumer_key => $consumer_key,
+	consumer_secret => $consumer_secret,
+	access_token => $access_token,
+	access_token_secret => $access_token_secret
+);
+
+eval {
+	$twitter->update($tweet)
+};
+if ($@) {
+	warn "Update failed: $@\n";
+}
+else {
+	# move $firstfile somewhere else
+}
