37f849a9831f896a4b902ebdc1f8a3dbcbdede6d
mpartridge <halcyon22@gmail.com>
2011-11-12 23:14:36 -0600

    even out the last two tweets for a line if the last will be less than 70 characters

--- src/splitfile.pl
+++ src/splitfile.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
 
+# no tweet will span lines;
+# the input should be one line per paragraph
+
 use warnings;
 use strict;
 use File::Path;
@@ -14,12 +17,14 @@ USAGE
 
 my $filename = shift(@ARGV);
 $filename =~ /^(.+)\..*$/;
-my $prefix = $1 or die "Couldn't parse $filename\n";
+my $prefix = $1 
+	or die "Couldn't parse $filename\n";
 my $outfiletmpl = $prefix."___NUM__.tweet";
 
-print "filename=$filename\n";
+#print "filename=$filename\n";
 
-open(INPUT, $filename) or die "Couldn't open $filename: $!\n";
+open(INPUT, $filename) 
+	or die "Couldn't open $filename: $!\n";
 
 my $characters = "";
 my $outfilecount = 0;
@@ -27,7 +32,7 @@ while (<INPUT>) {
 	my $line = $_;
 	chomp($line);
 
-	print "$line\n";
+	#print "$line\n";
 
 	my @words = split(/\s+/, $line);
 
@@ -37,19 +42,45 @@ while (<INPUT>) {
 
 		$characters .= "$word ";
 
-		print "$characters >>".length($characters)."\n";
+		#print "$characters >>".length($characters)."\n";
 
 		if (length($characters) >= 140) {
 			$characters = $prev;
 			unshift(@words, $word);
 
-			$outfilecount = tofile($characters, $outfiletmpl, $outfilecount);
+			# next tweet will be too short; even out the last two
+			if (length(join(' ', @words)) < 70) {
+				#print "next one is short; evening out\n";
 
-			$characters = "";
+				$characters .= join(' ', @words);
+				my $length = length($characters);
+				@words = split(/\s+/, $characters);
+
+				$characters = "";
+				while (length($characters) <= ($length / 2)) {
+					$characters .= shift(@words)." ";
+				}
+
+				# next to last tweet for this paragraph
+				$outfilecount = tofile($characters, $outfiletmpl, $outfilecount);
+				$characters = "";
+
+				while (@words) {
+					$characters .= shift(@words)." ";
+				}
+
+				# last tweet for this paragraph
+				$outfilecount = tofile($characters, $outfiletmpl, $outfilecount);
+				$characters = ""; 
+			}
+			else {
+				$outfilecount = tofile($characters, $outfiletmpl, $outfilecount);
+				$characters = "";
+			}
 		}
 	}
 
-	print "after\n";
+	#print "after\n";
 
 	if ($characters) {
 		$outfilecount = tofile($characters, $outfiletmpl, $outfilecount);
@@ -64,7 +95,7 @@ sub tofile {
 	my ($content, $tmpl, $count) = @_;
 	$tmpl =~ s/__NUM__/$count/;
 
-	print "Writing $tmpl\n$content\n";
+	#print "Writing $tmpl\n$content\n";
 
 	write_file($tmpl, "$content\n\n\n")
 		or die "Couldn't write $tmpl: $!\n";
